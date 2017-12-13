c833096653aee3d581ffea8e563cd01193cfcd3f
dg <dargrab@protonmail.com>
2017-12-13 07:47:05 +0100

    host w firebase za pomoca firebase cli

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBrZwlDzK11XMBeIrcQg0A75tICFwG99kQ',
+    authDomain: 'contacts-71eaa.firebaseapp.com',
+    databaseURL: 'https://contacts-71eaa.firebaseio.com',
+    projectId: 'contacts-71eaa',
+    storageBucket: 'contacts-71eaa.appspot.com',
+    messagingSenderId: '662925280283'
+  }
 };
