020a9d8f7ea4149e91c36389f193ac5d58aaddd9
dg <dargrab@protonmail.com>
2017-12-21 08:35:44 +0100

    logowanie sie i zapisanie danych o uzytkowniku do bazy

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {Observable} from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
+import {AngularFirestore, AngularFirestoreDocument} from 'angularfire2/firestore';
 
 @Injectable()
 export class AuthService {
@@ -10,20 +11,121 @@ export class AuthService {
 
   constructor(
     private router: Router,
+    private afs: AngularFirestore,
     private afAuth: AngularFireAuth
   ) {
     this.user$ = this.afAuth.authState;
   }
 
-  login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider )
-      .then( _ => this.router.navigate([`/company-list`]))
-      .catch( error => console.log('auth error', error) );
+  googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return this.oAuthLogin(provider, 'google');
   }
+
+  githubLogin() {
+    const provider = new firebase.auth.GithubAuthProvider();
+    return this.oAuthLogin(provider, 'github');
+  }
+
+  facebookLogin() {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    return this.oAuthLogin(provider, 'facebook');
+  }
+
+  twitterLogin() {
+    const provider = new firebase.auth.TwitterAuthProvider();
+    return this.oAuthLogin(provider, 'twitter');
+  }
+
+  private oAuthLogin(provider: firebase.auth.AuthProvider, providerName: string) {
+    return this.afAuth.auth.signInWithPopup(provider)
+      .then((credential) => {
+        // this.notify.update('Welcome to Firestarter!!!', 'success');
+        return this.updateUserData(credential.user, providerName);
+      })
+      .catch((error) => this.handleError(error) );
+  }
+
+
+  //// Anonymous Auth ////
+
+  anonymousLogin() {
+    return this.afAuth.auth.signInAnonymously()
+      .then((user) => {
+        // this.notify.update('Welcome to Firestarter!!!', 'success');
+        return this.updateUserData(user, 'anonymous'); // if using firestore
+      })
+      .catch((error) => {
+        console.error(error.code);
+        console.error(error.message);
+        this.handleError(error);
+      });
+  }
+
+
+  //// Email/Password Auth ////
+
+  emailSignUp(email: string, password: string) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        // this.notify.update('Welcome to Firestarter!!!', 'success');
+        return this.updateUserData(user, 'email'); // if using firestore
+      })
+      .catch((error) => this.handleError(error) );
+  }
+
+  emailLogin(email: string, password: string) {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        // this.notify.update('Welcome to Firestarter!!!', 'success')
+        return this.updateUserData(user, 'email'); // if using firestore
+      })
+      .catch((error) => this.handleError(error) );
+  }
+
+  // Sends email allowing user to reset password
+  resetPassword(email: string) {
+    const fbAuth = firebase.auth();
+
+    return fbAuth.sendPasswordResetEmail(email)
+      // .then(() => this.notify.update('Password update email sent', 'info'))
+      .catch((error) => this.handleError(error));
+  }
+
+
+
+
+
+  // login() {
+  //   this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider )
+  //     .then( _ => this.router.navigate([`/company-list`]))
+  //     .catch( error => console.log('auth error', error) );
+  // }
   logout() {
     this.afAuth.auth.signOut();
     this.router.navigate([`/home`]);
   }
 
 
+
+  // Sets user data to firestore after succesful login
+  private updateUserData(user: User, providerName: string) {
+
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+
+    const data: User = {
+      uid: user.uid,
+      email: user.email || null,
+      displayName: user.displayName || 'nameless user',
+      photoURL: user.photoURL || 'https://goo.gl/Fz9nrQ',
+      providerName: providerName
+    };
+    return userRef.set(data);
+  }
+
+  // If error, console log and notify user
+  private handleError(error: Error) {
+    console.error(error);
+    // this.notify.update(error.message, 'error');
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService,
+              private router: Router) { }
+
+  signInWithGithub() {
+    this.auth.githubLogin()
+      .then(() => this.afterSignIn());
+  }
+
+  signInWithGoogle() {
+    this.auth.googleLogin()
+      .then(() => this.afterSignIn());
+  }
+
+  signInWithFacebook() {
+    this.auth.facebookLogin()
+      .then(() => this.afterSignIn());
+  }
+
+  signInWithTwitter() {
+    this.auth.twitterLogin()
+      .then(() => this.afterSignIn());
+  }
+
+  /// Anonymous Sign In
+
+  signInAnonymously() {
+    this.auth.anonymousLogin()
+      .then(() => this.afterSignIn());
+  }
+
+  /// Shared
+
+  private afterSignIn() {
+    // Do after login stuff here, such router redirects, toast messages, etc.
+    this.router.navigate(['/']);
+  }
+
 
   ngOnInit() {
   }
